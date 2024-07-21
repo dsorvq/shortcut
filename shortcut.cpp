3cd2612eddb702373ca0e0c41e116cd9fd000dc0
dsorvq <dsorvq@gmail.com>
2024-07-21 18:37:24 +0300

    Add a temporary transposed matrix for better cache access

--- shortcut.cpp
+++ shortcut.cpp
@@ -4,13 +4,21 @@
 
 matrix shortcut(const matrix& distances) {
   const int n = distances.size();
+
+  matrix trasnposed(n, matrix_row(n));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      trasnposed[j][i] = distances[i][j];
+    }
+  }
+  
   matrix result(n, matrix_row(n));
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
       auto path_ij = std::numeric_limits<float>::infinity();
       for (int k = 0; k < n; ++k) {
         auto x = distances[i][k];
-        auto y = distances[k][j];
+        auto y = trasnposed[j][k];
         auto z = x + y;
         path_ij = std::min(path_ij, z);
       }
