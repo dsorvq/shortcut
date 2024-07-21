051141c369c3d17b1f4c6852face29dc89947e9c
dsorvq <dsorvq@gmail.com>
2024-07-21 17:46:56 +0300

    Add simple TimeGuard to measure time

---
+++ timer-guard.hpp
@@ -0,0 +1,29 @@
+#include <random>
+#include <string>
+#include <iostream>
+#include <chrono>
+
+class TimerGuard {
+public:
+  TimerGuard(std::string message = "", std::ostream& out = std::cout)
+    : start(std::chrono::high_resolution_clock::now())
+    , outMessage(message)
+    , outStream(out)
+  { }
+
+  ~TimerGuard() {
+    auto end = std::chrono::high_resolution_clock::now();  // конец - вызов деструктора
+    std::chrono::duration<double> diff = end - start;
+    // auto diffns = std::chrono::duration_cast<std::chrono::nanoseconds> (diff);
+    auto diffms = std::chrono::duration_cast<std::chrono::milliseconds> (diff);
+    outStream << outMessage << " " 
+      // << diffns.count() << "ns" << " == "
+      << diffms.count() << "ms" << " == "
+      << diff.count() << "s" << "\n";
+  }
+
+private:
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
+  std::string outMessage;
+  std::ostream& outStream;
+};
