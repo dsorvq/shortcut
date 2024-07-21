c2230200fd47fa4c8dff24004ea120695cac79d9
dsorvq <dsorvq@gmail.com>
2024-07-21 18:22:55 +0300

    Add initial tests for correctness & performance

--- main.cpp
+++ main.cpp
@@ -6,45 +6,66 @@
 #include "shortcut.hpp"
 #include "timer-guard.hpp"
 
+void test_correctness();
+void test_performance(auto alorithm, int n);
+
 int main() {
 #ifdef TEST
-  constexpr int n = 3;
-  const matrix d  = {
-    { 0, 8, 2 }, 
-    { 1, 0, 9 },
-    { 4, 5, 0 },
-  };
-  assert(d.size() == n);
-  assert(d[0].size() == n);
-  
-  auto result = shortcut(d);
-
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      std::cout << result[i][j] << " ";
-    }
-    std::cout << "\n";
-  }
+  test_correctness();
 #else
-  constexpr int n = 2000;
-  matrix d(n, matrix_row(n));
+  test_performance(shortcut, 2000);
+#endif
+} 
 
-  // Fill d with random values
+matrix get_random_square_matrix(int n) {
+  matrix mat(n, matrix_row(n));
+
   std::random_device rd;
   std::default_random_engine re(rd());
   std::uniform_real_distribution<float> ud(0.0, 1.0);
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      d[i][j] = ud(re);
+      mat[i][j] = ud(re);
     }
   }
+
+  return mat; 
+}
+
+void test_correctness() {
+  // just some sanity checks
+
+  {
+    constexpr int n = 3;
+    const matrix d  = {
+      { 0, 8, 2 }, 
+      { 1, 0, 9 },
+      { 4, 5, 0 },
+    };
+    assert(d.size() == n);
+    assert(d[0].size() == n);
+    
+    assert(baseline(d) == baseline(d)); 
+  }
  
-  // Perfrom algorithm
+  auto test_for_n = [](int n, int iterations=10) {
+    for (int i = 0; i < iterations; ++i) {
+      auto mat = get_random_square_matrix(n);
+      auto a = shortcut(mat);
+      auto b = baseline(mat);
+      assert(a == b);
+    }
+  };
+
+  test_for_n(3);
+  test_for_n(4);
+}
+
+void test_performance(auto alorithm, int n) {
+  auto mat = get_random_square_matrix(n);
+
   {
     TimerGuard tg("time:");
-    auto result = baseline(d);
+    auto result = alorithm(mat);
   }
-
-#endif
-} 
-
+}
